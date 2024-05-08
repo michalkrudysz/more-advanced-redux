228599914153e88e621321032e094118b5269938
Michal <michal@drontrzebownisko.pl>
2024-05-08 14:09:39 +0200

    Implemented error handling, data submission notifications, and successful data transmission alerts within the application. A dedicated component was developed to display these messages to the user.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+import { Fragment } from "react";
 
 function App() {
+  const dispatch = useDispatch(); // useDispatch pozwala na wywołanie akcji z store
   const showCart = useSelector((state) => state.ui.cartIsVisible); // useSelector pozwala na pobranie stanu z store (w tym przypadku cartIsVisible)
   const cart = useSelector((state) => state.cart); // useSelector pozwala na pobranie stanu z store (w tym przypadku cart)
+  const notification = useSelector((state) => state.ui.notification); // useSelector pozwala na pobranie stanu z store (w tym przypadku notification)
 
   useEffect(() => {
-    fetch(
-      "https://learning-redux-b386b-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-      {
-        method: "PUT",
-        body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://learning-redux-b386b-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
       }
-    );
-  }, [cart]);
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,13 +2,20 @@ import { createSlice } from "@reduxjs/toolkit"; // pobieram z redux toolkit funk
 
 const uiSlice = createSlice({
   name: "ui", // nazwa slice
-  initialState: { cartIsVisible: false }, // stan początkowy
+  initialState: { cartIsVisible: false, notification: null }, // stan początkowy
   // mapa metod dla funkcji reducera
   reducers: {
     // metoda toogle przyjmuje stan i zmienia wartość cartIsVisible na przeciwną
     // warto podkreślić, że dzięki reduxtool, mogę tak zmienić stan na przeciwny, bez konieczności przekazywania całego stanu
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
