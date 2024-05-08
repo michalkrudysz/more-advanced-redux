de8dd5d30d17d91d10db26cfb895112e033cb285
Michal <michal@drontrzebownisko.pl>
2024-05-08 12:59:12 +0200

    Established a connection with Firebase to transfer the data structure (current shopping cart) created in Redux. The connection was initiated from App.js, where the current cart is retrieved and then transmitted to Firebase.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,6 +6,17 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible); // useSelector pozwala na pobranie stanu z store (w tym przypadku cartIsVisible)
+  const cart = useSelector((state) => state.cart); // useSelector pozwala na pobranie stanu z store (w tym przypadku cart)
+
+  useEffect(() => {
+    fetch(
+      "https://learning-redux-b386b-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
 
   return (
     <Layout>
